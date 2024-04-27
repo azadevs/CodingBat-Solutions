cedcf05ce6b804e75845ba987c72001b1ed95c2d
Azamat <azamatdeveloper3@gmail.com>
2024-04-27 12:16:41 +0500

    Solved sumLimit problem in Logic-1

---
+++ src/logic_1/SumLimit.java
@@ -0,0 +1,17 @@
+package logic_1;
+
+import static base.Base.sc;
+
+public class SumLimit {
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(sumLimit(a, b));
+    }
+
+    public static int sumLimit(int a, int b) {
+        String sum = String.valueOf(a + b);
+        String strA = String.valueOf(a);
+        return sum.contains(strA) ? a : sum.length() > strA.length() ? a : Integer.parseInt(sum);
+    }
+}
