7de6c8c766834560a9c15f61224c895126f92eb2
Azamat <azamatdeveloper3@gmail.com>
2024-04-30 11:10:43 +0500

    Solved count7 problem in Recursion-1

---
+++ src/recursion_1/Count7.java
@@ -0,0 +1,16 @@
+package recursion_1;
+
+import static base.Base.sc;
+
+public class Count7 {
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        System.out.println(count7(n));
+    }
+
+    public static int count7(int n) {
+        if (n == 7) return 1;
+        else if (n < 10) return 0;
+        return n % 10 == 7 ? 1 + count7(n / 10) : count7(n / 10);
+    }
+}
