4213961a6befa3d8fbc2e304fa696a16ef4b97d5
Azamat <azamatdeveloper3@gmail.com>
2024-04-30 11:18:48 +0500

    Improved code quality

--- src/recursion_1/Count7.java
+++ src/recursion_1/Count7.java
@@ -9,8 +9,7 @@ public class Count7 {
     }
 
     public static int count7(int n) {
-        if (n == 7) return 1;
-        else if (n < 10) return 0;
+        if (n == 0) return 0;
         return n % 10 == 7 ? 1 + count7(n / 10) : count7(n / 10);
     }
 }
