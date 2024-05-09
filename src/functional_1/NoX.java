bcd6169f4e230e5f38f48a5bf1ed6b5340670637
Azamat <azamatdeveloper3@gmail.com>
2024-05-09 21:08:33 +0500

    Solved noX problem in Functional-1

---
+++ src/functional_1/NoX.java
@@ -0,0 +1,19 @@
+package functional_1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NoX {
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            strings.add("asxax" + i);
+        }
+        System.out.println(noX(strings));
+    }
+
+    public static List<String> noX(List<String> strings) {
+        return strings.stream().map(e -> e.replace("x", "")).collect(Collectors.toList());
+    }
+}
