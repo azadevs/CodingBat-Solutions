e7d4cdd46d2305c3460b2980679e3d2d2b7aa74d
Azamat <azamatdeveloper3@gmail.com>
2024-05-29 12:51:29 +0500

    Solved makeChocolate problem in Logic-2

---
+++ src/logic_2/MakeChocolate.java
@@ -0,0 +1,24 @@
+package logic_2;
+
+import static base.Base.sc;
+
+public class MakeChocolate {
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println(makeChocolate(a, b, c));
+    }
+
+    public static int makeChocolate(int small, int big, int goal) {
+        int res;
+        if (goal >= 5 * big) {
+            res = goal - 5 * big;
+        } else {
+            res = goal - 5 * (goal / 5);
+        }
+        if (small >= res) return res;
+        return -1;
+    }
+}
