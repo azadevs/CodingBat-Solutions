5bb4b8faf503dd1784e90ee805ca570c72055e37
Azamat <azamatdeveloper3@gmail.com>
2024-06-12 15:54:58 +0500

    Completed Functional-2 problems

--- src/functional_2/Square56.java
+++ src/functional_2/Square56.java
@@ -7,4 +7,5 @@ public class Square56 {
     public List<Integer> square56(List<Integer> nums) {
         return nums.stream().map(n -> n * n + 10).filter(n -> n % 10 != 5 && n % 10 != 6).collect(Collectors.toList());
     }
+
 }
