23ee6b9f0b55aeca480b3209722cb93dacf3c67f
Azamat <azamatdeveloper3@gmail.com>
2024-06-12 15:51:29 +0500

    Solved two2 problem in Functional-2

---
+++ src/functional_2/Two2.java
@@ -0,0 +1,10 @@
+package functional_2;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Two2 {
+    public List<Integer> two2(List<Integer> nums) {
+        return nums.stream().map(n -> 2 * n).filter(n -> n % 10 != 2).collect(Collectors.toList());
+    }
+}
