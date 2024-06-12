6f3156cc3886373c7dfe0bf9221e6778ba4ed64b
Azamat <azamatdeveloper3@gmail.com>
2024-06-12 15:42:48 +0500

    Solved noZ problem in Functional-2

---
+++ src/functional_2/NoZ.java
@@ -0,0 +1,10 @@
+package functional_2;
+
+import java.util.List;
+
+public class NoZ {
+    public List<String> noZ(List<String> strings) {
+        strings.removeIf(s -> s.contains("z"));
+        return strings;
+    }
+}
