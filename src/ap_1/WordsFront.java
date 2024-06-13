54eaadcea7c24691870dc3b46718a843f4adff7d
Azamat <azamatdeveloper3@gmail.com>
2024-06-13 11:56:59 +0500

    Solved wordsFront problem in AP-1

---
+++ src/ap_1/WordsFront.java
@@ -0,0 +1,11 @@
+package ap_1;
+
+public class WordsFront {
+    public String[] wordsFront(String[] words, int n) {
+        String[] newWords = new String[n];
+        for (int i = 0; i < n; i++) {
+            newWords[i] = words[i];
+        }
+        return newWords;
+    }
+}
