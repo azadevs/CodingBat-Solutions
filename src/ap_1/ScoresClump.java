5af2e475f2b0658752e679459393b0522fc11861
Azamat <azamatdeveloper3@gmail.com>
2024-06-13 11:21:30 +0500

    Solved scoresClump problem in AP-1

---
+++ src/ap_1/ScoresClump.java
@@ -0,0 +1,13 @@
+package ap_1;
+
+public class ScoresClump {
+    public boolean scoresClump(int[] scores) {
+        for (int i = 0; i < scores.length - 2; i++) {
+            if (scores[i + 1] - scores[i] <= 2 && scores[i + 2] - scores[i + 1] <= 2
+                    && scores[i + 2] - scores[i] <= 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
