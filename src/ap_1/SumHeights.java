b70c08da08974721ef41bc2f8df3321941a55b53
Azamat <azamatdeveloper3@gmail.com>
2024-06-17 10:28:08 +0500

    Optimized sumHeights2 problem in AP-1

--- src/ap_1/SumHeights.java
+++ src/ap_1/SumHeights.java
@@ -3,10 +3,8 @@ package ap_1;
 public class SumHeights {
     public int sumHeights(int[] heights, int start, int end) {
         int sum = 0;
-        for (int i = 0; i < heights.length - 1; i++) {
-            if (i >= start && i + 1 <= end) {
+        for (int i = start; i < end; i++) {
                 sum += Math.abs(heights[i] - heights[i + 1]);
-            }
         }
         return sum;
     }
