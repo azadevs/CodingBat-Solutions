17f30869a41fea5a76ba1f4c33e4fe3724e51420
Azamat <azamatdeveloper3@gmail.com>
2024-06-21 08:39:46 +0500

    Solved commonTwo problem in AP-1

---
+++ src/ap_1/CommonTwo.java
@@ -0,0 +1,14 @@
+package ap_1;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CommonTwo {
+    public int commonTwo(String[] a, String[] b) {
+        Set<String> stringSetA = new HashSet<>(Arrays.asList(a));
+        Set<String> stringSetB = new HashSet<>(Arrays.asList(b));
+        stringSetA.retainAll(stringSetB);
+        return stringSetA.size();
+    }
+}
