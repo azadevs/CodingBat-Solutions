b869f954170689d8c8d968d6c4b76cd582ee7643
Azamat <azamatdeveloper3@gmail.com>
2024-06-21 08:42:57 +0500

    Solved countEvens problem in Array_2

---
+++ src/array_2/CountEvens.java
@@ -0,0 +1,11 @@
+package array_2;
+
+public class CountEvens {
+    public int countEvens(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            if (num % 2 == 0) count++;
+        }
+        return count;
+    }
+}
