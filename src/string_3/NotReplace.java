3c05cded83852cffce2c1ff60c63345c764d2dfa
Azamat <azamatdeveloper3@gmail.com>
2024-07-21 19:49:18 +0500

    Solved notReplace problem in String-3

---
+++ src/string_3/NotReplace.java
@@ -0,0 +1,19 @@
+package string_3;
+
+public class NotReplace {
+    public String notReplace(String str) {
+        str = " " + str + " ";
+        StringBuilder newStr = new StringBuilder();
+        for (int i = 0; i < str.length() - 1; i++) {
+            String temp = str.substring(i, i + 2);
+            if (temp.equals("is") && !Character.isLetter(str.charAt(i - 1)) && !Character.isLetter(str.charAt(i + 2))) {
+                newStr.append("is not");
+                i++;
+            } else {
+                newStr.append(str.charAt(i));
+            }
+        }
+        return newStr.substring(1);
+    }
+}
+
